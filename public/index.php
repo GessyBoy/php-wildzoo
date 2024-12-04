d7cb6bb67ab444ffbc675057c95ab2d5a1d0320d
GessyBoy <ydeletrez@gmail.com>
2024-12-04 17:24:41 +0100

    exercice 2 done

--- public/index.php
+++ public/index.php
@@ -5,11 +5,30 @@
 
 require __DIR__ . '/../src/Animal.php';
 
-$animal1 = new Animal();
-$animal2 = new Animal();
+$lion = new Animal();
+$lion->name = 'lion';
+$lion->size = 50;
+$lion->pawNumber = 4;
+$lion->carnivorous = true;
+$lion->threatenedLevel = 'VU';
 
-$animals = [$animal1, $animal2];
+$parrot = new Animal();
+$parrot->name = 'parrot';
+$parrot->size = 20;
+$parrot->pawNumber = 2;
+$parrot->threatenedLevel = 'LC';
 
+$lioncub = new Animal();
+$lioncub->name = 'lioncub';
+$lioncub->size = 10;
+$lioncub->pawNumber = 4;
+$lioncub->carnivorous = true;
+$lioncub->threatenedLevel = 'VU';
+
+
+$animals = [$lion, $parrot, $lioncub];
+
+echo 'Bonjour je suis un ' . $lion->name . ' et j\'ai ' . $lion->pawNumber . ' patte(s)';
 /***************************************/
 /***************************************/
 
