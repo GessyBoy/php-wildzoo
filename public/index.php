43ac02d42a845790b3f56c1cab9f5a7bdc0c0833
GessyBoy <ydeletrez@gmail.com>
2024-12-08 16:11:34 +0100

    Exercice 7 done

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@
 /******** ⚠️ WORK HERE ONLY ⚠️ ***********/
 
 require __DIR__ . '/../src/Animal.php';
+require __DIR__ . '/../src/Area.php';
+
+use App\Animal;
+use App\Area;
 
 $lion = new Animal('lion', 4);
 $lion->setCarnivorous(true);
@@ -18,6 +22,15 @@ $elephant->setThreatenedLevel('LC');
 
 $animals = [$lion, $parrot, $elephant];
 
+$savana = new Area('savana');
+$jungle = new Area('jungle');
+
+$savana->addAnimal($lion);
+$savana->addAnimal($elephant);
+
+$jungle->addAnimal($parrot);
+
+$areas = [$savana, $jungle];
 /***************************************/
 /***************************************/
 
